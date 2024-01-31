8b9fab6683b98e8652a6a143df3f616628514992
SugarSD <jdrice5250@gmail.com>
2024-01-30 17:20:51 -0700

    Initial Commit

---
+++ sugar-perks/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+//Import Components
+import Header from './components/Header/Header';
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div id="main">
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ sugar-perks/src/components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import './Header.css'
+
+function Header() {
+
+  return (
+    <>
+      <div id="header">
+        <div id="header-dropdown">
+          <i class="fa-solid fa-bars"></i>
+        </div>
+        <div id="header-title">
+          <h1>Sugar <span>Perks</span></h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header;
